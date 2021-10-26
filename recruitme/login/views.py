bc2b8a1fde4b29e7ceb9e4bdbffce0dd04af3f91
OmkarDhekane <omkarjd1212@gmail.com>
2021-10-26 23:17:38 +0530

    Login | signup logic added

--- recruitme/login/views.py
+++ recruitme/login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ApplicantLoginForm, ApplicantSignUpForm
-
+from .models import ApplicantSignedUp, ApplicantLoggedIn
 
 # Create your views here.
 
@@ -15,11 +15,26 @@ def loginSignup(request):
             print("login form created!")
             username = login_form.cleaned_data['username']
             password = login_form.cleaned_data['password']
-            print(username)
-            print(password)
             # time to save these into mysql database
             # form.save()
-            return redirect('dashboard')  # should redirect to dashboard
+
+            # select 1 as id acts as  primary key.
+            # query = "select 1 as id,exists(select * from login_applicantsignedup WHERE username='" + \
+            #     username+"' and newPassword='"+password+"') as isPresent;"
+            query = "select 1 as id, exists(select * from login_applicantloggedin WHERE username='" + \
+                username+"' and password='"+password+"') as isPresent;"
+
+            print(query)
+            for user in ApplicantSignedUp.objects.raw(query):
+                if user.isPresent:
+                    print("USER IS PRESENT")
+                    # should redirect to dashboard
+                    return redirect('dashboard')
+                else:
+                    print("USER IS NOT PRESENT")
+                    # should redirect to login page
+                    return redirect('login-signup-page')
+
         elif signup_form.is_valid():
             print("Signup form Created!")
             username = signup_form.cleaned_data['username']
@@ -30,6 +45,8 @@ def loginSignup(request):
             print(newPassword)
             # time to save these into mysql database
             signup_form.save()
+            ApplicantLoggedIn.objects.create(
+                username=username, password=newPassword)
             return redirect('profile')  # should redirect to profile
 
         else:
